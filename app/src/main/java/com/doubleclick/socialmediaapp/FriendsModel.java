20b2d9e85f284a2913280b072af7072f11b9a9ce
eslam <eslamfgazy600@gmail.com>
2022-02-24 16:58:01 +0200

    SocialMediaApp

---
+++ app/src/main/java/com/doubleclick/socialmediaapp/FriendsModel.java
@@ -0,0 +1,17 @@
+package com.doubleclick.socialmediaapp;
+
+public class FriendsModel {
+    int Profile;
+
+    public FriendsModel(int profile) {
+        Profile = profile;
+    }
+
+    public int getProfile() {
+        return Profile;
+    }
+
+    public void setProfile(int profile) {
+        Profile = profile;
+    }
+}
